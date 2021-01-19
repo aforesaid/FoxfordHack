e72cedeaf5147742d10b2f89e796edc88bc44b6a
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 00:55:35 +0300

    Add Logging Service

---
+++ FoxfordHack/Services/Logging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FoxfordHack.Services
+{
+    class Logging
+    {
+        private static readonly string tokenPath = Path.Combine(Directory.GetCurrentDirectory(), "logs.log");
+        public void FixTheError (Exception ex)
+            => File.AppendAllText(tokenPath,ex.ToString());
+        
+    }
+}
