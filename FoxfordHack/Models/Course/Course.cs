92443ba1ec66ca022e69cee32eca8407effe1947
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 00:56:44 +0300

    Add Model Element For Course

---
+++ FoxfordHack/Models/Course/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoxfordHack.Models.Course
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public List<TaskFoxford> Tasks { get; set; }
+    }
+}
---
+++ FoxfordHack/Models/Course/TaskFoxford.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoxfordHack.Models.Course
+{
+    public class TaskFoxford
+    {
+        public int Id { get; set; }
+        [ForeignKey("CourseKey")]
+        public int CourseId { get; set; }
+        public int TaskFoxfordId { get; set; }
+        public string Answer { get; set; }
+    }
+}
