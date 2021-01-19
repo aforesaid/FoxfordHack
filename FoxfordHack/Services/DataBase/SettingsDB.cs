9b6f1498b2098559a79ba7ed083b7bc1e802b5f6
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 00:57:05 +0300

    Add SettingsDB Service

---
+++ FoxfordHack/Services/DataBase/SettingsDB.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace FoxfordHack.Services.DataBase
+{
+    public class SettingsDB
+    {
+        public static readonly string ConfigurationJsonFile = "appsettings.json";
+        public string GetConnectionString()
+        {
+                var builder = new HostBuilder();
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config
+                           .AddJsonFile(ConfigurationJsonFile, true)
+                           .AddEnvironmentVariables();
+                });
+
+                var host = builder.Build();
+                var config = host.Services.GetService<IConfiguration>();
+                return config.GetConnectionString("Default");
+        }
+    }
+}
+
