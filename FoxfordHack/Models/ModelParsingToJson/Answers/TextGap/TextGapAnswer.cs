0512b97b275e4cb5ccbb9863dbca15447949cdc9
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 23:21:42 +0300

    Add JsonModel TextGapAnswer

---
+++ FoxfordHack/Models/ModelParsingToJson/Answers/TextGap/TextGapAnswer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization ;
+
+namespace FoxfordHack.Models.ModelParsingToJson.Answers.TextGap
+{
+    class TextGapAnswer
+    {
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("identifier")]
+        public string Identifier { get; set; }
+        [JsonPropertyName("correct")]
+        public bool IsCorrect { get; set; }
+    }
+}
