f8cd7f0c77e2a8e463fdb2febc7242b6e3c0757f
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 23:21:16 +0300

    Add JsonModel CheckboxAnswer

---
+++ FoxfordHack/Models/ModelParsingToJson/Answers/Checkbox/CheckboxAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoxfordHack.Models.ModelParsingToJson.Answers.CheckboxType
+{
+    class CheckboxAnswer
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("correct")]
+        public bool IsCorrect { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+        [JsonPropertyName("content_type")]
+        public string ContentType { get; set; }
+    }
+}
