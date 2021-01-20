544c1c4234433203f3b72a06dd14a211c881c120
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 23:24:42 +0300

    Add Model User for WebApi

---
+++ FoxfordHack/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+namespace FoxfordHack.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; } 
+        public List<Course.Course> Courses { get; set; }
+    }
+}
