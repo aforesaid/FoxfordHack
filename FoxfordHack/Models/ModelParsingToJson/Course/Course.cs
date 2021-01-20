bee24f5c4a4bac0d25431535836de99bff05139e
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 23:22:16 +0300

    Add JsonModel for Course

---
+++ FoxfordHack/Models/ModelParsingToJson/Course/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+namespace FoxfordHack.Models.ModelParsingToJson.Course
+{
+    class Course
+    {
+        [JsonPropertyName("resource_id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("cart_item_type")]
+        public string CartItemType { get; set; }
+    }
+}
---
+++ FoxfordHack/Models/ModelParsingToJson/Course/ViewCourses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FoxfordHack.Models.ModelParsingToJson.Course
+{
+    class ViewCourses
+    {
+        [JsonPropertyName("bookmarks")]
+        public List<Course> AciveCourses { get; set; }
+    }
+}
