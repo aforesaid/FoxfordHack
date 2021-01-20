e8194da82b8412086d2b65f59a9fd523deb1ad5b
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-20 23:25:41 +0300

    Add Service from Convert JsonAnswer to Model

---
+++ FoxfordHack/Services/ConvertersJsonToModel/ConverterAnswerToModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using FoxfordHack.Models.ModelParsingToJson.Answers;
+namespace FoxfordHack.Services.ConvertersJsonToModel
+{
+    class ConverterAnswerToModel<T>
+    {
+        public List<T> ConvertObjectJsonToModel(object Info)
+        {
+            try
+            {
+                var jsonInfo = JsonSerializer.Serialize(Info);
+                var list = JsonSerializer.Deserialize<List<T>>(jsonInfo);
+                return list;
+            }
+            catch (Exception ex)
+            {
+                new Logging().FixTheError(ex);
+                return null;
+            }
+        }
+        public Type GetTypeByEnum(ANSWER_TYPES type)
+            => type switch
+            {
+
+                _ => throw new ArgumentException("Bad type")
+            };
+    }
+}
