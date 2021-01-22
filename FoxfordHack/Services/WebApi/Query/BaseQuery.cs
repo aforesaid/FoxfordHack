a8efde34db6097d570fc16c68f35bf5bd527d69a
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:52:48 +0300

    Add Service BaseQuery

--- FoxfordHack/Services/WebApi/Query/BaseQuery.cs
+++ FoxfordHack/Services/WebApi/Query/BaseQuery.cs
@@ -5,6 +5,7 @@
         protected static readonly string DefaultUserAgent = @"Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)";
         protected static readonly string DefaultURL = @"https://foxford.ru";
         public string Cookie { get; protected set; }
+        public string XCSRFToken { get; protected set; }
         public int CountThreads { get; protected set; }
         public int Delay { get; protected set; }
     }
