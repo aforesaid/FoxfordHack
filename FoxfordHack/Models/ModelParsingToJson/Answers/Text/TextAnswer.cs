9edd53256b7f93a291f57cd8fcdedc17d8a895f1
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:49:44 +0300

    Add TextAnswer json model

---
+++ FoxfordHack/Models/ModelParsingToJson/Answers/Text/TextAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization ;
+
+namespace FoxfordHack.Models.ModelParsingToJson.Answers.Text
+{
+    class TextAnswer
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("correct_answers")]
+        public List<string> CorrectAnswers { get; set; }
+    }
+}
