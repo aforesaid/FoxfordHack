dc86c938247b48b05277fd20b9b3be8dc7c5ae5f
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:50:41 +0300

    Refactoring TaskFoxford

--- FoxfordHack/Models/ModelParsingToJson/TaskFoxford.cs
+++ FoxfordHack/Models/ModelParsingToJson/TaskFoxford.cs
@@ -1,7 +1,13 @@
-﻿namespace FoxfordHack.Models.ModelParsingToJson
+﻿using System.Text.Json.Serialization;
+namespace FoxfordHack.Models.ModelParsingToJson
 {
     class TaskFoxford
     {
-
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
     }
 }
