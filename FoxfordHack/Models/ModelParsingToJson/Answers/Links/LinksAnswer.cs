11b53e2b7897463e62e47f3227387d4002cfe913
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:49:10 +0300

    Add Links Answer json model

---
+++ FoxfordHack/Models/ModelParsingToJson/Answers/Links/LinksAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FoxfordHack.Models.ModelParsingToJson.Answers.Links
+{
+    class LinksAnswer
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("correct_answer_ids")]
+        public List<int> CorrectAnswersId { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+}
