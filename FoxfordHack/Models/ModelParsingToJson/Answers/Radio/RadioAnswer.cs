cb1a3f90b2e3a2e4272258b6bf3d99659b530f47
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:49:25 +0300

    Add RadioAnswer json model

---
+++ FoxfordHack/Models/ModelParsingToJson/Answers/Radio/RadioAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+namespace FoxfordHack.Models.ModelParsingToJson.Answers.Radio
+{
+    class RadioAnswer
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+        [JsonPropertyName("content_type")]
+        public string ContentType { get; set; }
+        [JsonPropertyName("correct")]
+        public bool IsCorrect { get; set; }
+    }
+}
