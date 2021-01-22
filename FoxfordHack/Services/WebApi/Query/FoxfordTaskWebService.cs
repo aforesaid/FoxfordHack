dfc6aca0ad73d020b5d0e9e5ebf5d88ac2a2ce1c
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 16:56:10 +0300

    Add multithreading method from Foxford TaskWebService

--- FoxfordHack/Services/WebApi/Query/FoxfordTaskWebService.cs
+++ FoxfordHack/Services/WebApi/Query/FoxfordTaskWebService.cs
@@ -10,7 +10,7 @@ namespace FoxfordHack.Services.WebApi.Query
     class FoxfordTaskWebService : BaseQuery
     {
         private static readonly string DefaultURLForTasks = @"https://foxford.ru/lessons/";
-        public FoxfordTaskWebService(string cookie,string token, int countThreads = 10, int delay = 500)
+        public FoxfordTaskWebService(string cookie, string token, int countThreads = 10, int delay = 500)
         {
             Cookie = cookie;
             CountThreads = countThreads;
@@ -28,28 +28,56 @@ namespace FoxfordHack.Services.WebApi.Query
             var result = JsonSerializer.Deserialize<List<TaskFoxford>>(jsonString);
             return result;
         }
-        public async Task<ViewQuestionPage> GetAnswerFromTask(int lessonId, int taskId,string status)
+        public async Task<ViewQuestionPage> GetAnswerFromTask(TaskFoxford task, int lessonId)
         {
-            var url = $"{DefaultURL}/api/lessons/{lessonId}/tasks/{taskId}";
-
+            if (task is null)
+                return null;
+            var url = $"{DefaultURL}/api/lessons/{lessonId}/tasks/{task.Id}";
             using var client = new HttpClient();
             client.DefaultRequestHeaders.Add("Cookie", Cookie);
             client.DefaultRequestHeaders.Add("User-Agent", DefaultUserAgent);
             client.DefaultRequestHeaders.Add("X-CSRF-Token", XCSRFToken);
-            if (status != "fail")
-            {
-                //Fails task
-                var request = await client.PostAsync($"{url}/fails", null);
-                if (request.StatusCode != System.Net.HttpStatusCode.OK)
-                {
-                    Console.WriteLine($"\nRequest Status Code is {request.StatusCode}");
-                }
-            }
-            //Get answers from task
             var requests = await client.GetAsync(url);
             var jsonString = await requests.Content.ReadAsStringAsync();
             var result = JsonSerializer.Deserialize<ViewQuestionPage>(jsonString);
             return result;
+        }
+        public void FailsAnswerFromTaskByLesson(List<TaskFoxford> listTasks, int lessonId)
+        {
+            if (listTasks is null)
+                return;
+            var tasks = new Task[listTasks.Count];
+            for (int i = 0; i < CountThreads; i++)
+            {
+                tasks[i] = FailsAnswerFromTasks(listTasks, lessonId, i);
+            }
+            Task.WaitAll(tasks);
+        }
+        private async Task FailsAnswerFromTasks(List<TaskFoxford> tasks, int lessonId,int startIndex)
+        {
+            for (int i = startIndex; i < tasks.Count; i+= CountThreads)
+            {
+                await FailsAnswerFromTask(tasks[i],lessonId);
+            }
+        }
+        public async Task<bool> FailsAnswerFromTask(TaskFoxford task,int lessonId)
+        {
+            if (task.Status is "fail")
+                return false;
+            var url = $"{DefaultURL}/api/lessons/{lessonId}/tasks/{task.Id}/fails";
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Cookie", Cookie);
+            client.DefaultRequestHeaders.Add("User-Agent", DefaultUserAgent);
+            client.DefaultRequestHeaders.Add("X-CSRF-Token", XCSRFToken);
+            var request = await client.PostAsync(url, null);
+            if (request.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"Status Code is {request.StatusCode},Links {url}");
+                await FailsAnswerFromTask(task, lessonId);
+            }
+            task.Status = "fail";
+            await Task.Delay(Delay);
+            return true;
         }
         public async Task<bool> SetAnswerForTask(int lessonId, int taskId,Dictionary<string,string> answer)
         {
