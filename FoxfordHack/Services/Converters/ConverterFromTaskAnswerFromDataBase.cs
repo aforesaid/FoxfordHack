f6928a28f16786d948a8882ffaa41f739ee9848e
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:51:50 +0300

    Add ConverterFromTaskAnswerFromDataBase

---
+++ FoxfordHack/Services/Converters/ConverterFromTaskAnswerFromDataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace FoxfordHack.Services.Converters
+{
+    public static class ConverterFromTaskAnswerFromDataBase
+    {
+        public static string DictionaryToString
+      (Dictionary<string, string> dictionary)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("Bad Dictionary");
+
+            var items = from kvp in dictionary
+                        select kvp.Key + "=" + kvp.Value;
+
+            return "{" + string.Join(",", items) + "}";
+        }
+        public static IDictionary<string, string> StringToDictionary
+      (string content)
+        {
+            if (content == null)
+                throw new ArgumentNullException("Bad string from DB");
+            var dictionary = new Dictionary<string, string>();
+            var contents = content.Split(new char[] { '=', '}', '{', ',' });
+            for (int i = 1; i+1 < contents.Length; i+=2)
+                dictionary.Add(contents[i], contents[i + 1]);
+            return dictionary;
+        }
+    }
+}
