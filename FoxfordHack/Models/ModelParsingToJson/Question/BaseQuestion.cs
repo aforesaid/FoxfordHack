7478691b2c76bb0ac6d47beebbc7f5d730319c9c
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-01-22 15:50:20 +0300

    Add Question json models

---
+++ FoxfordHack/Models/ModelParsingToJson/Question/BaseQuestion.cs
@@ -0,0 +1,18 @@
+﻿using FoxfordHack.Models.ModelParsingToJson.Answers;
+using System.Text.Json.Serialization;
+namespace FoxfordHack.Models.ModelParsingToJson.Question
+{
+    class BaseQuestion
+    {
+        [JsonPropertyName("answers")]
+        public object ObjectAnswers { get; set; }
+        [JsonPropertyName("critical_errors_count")]
+        public int MaxErrorCount { get; set; }
+        [JsonPropertyName("Header")]
+        public string Header { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
---
+++ FoxfordHack/Models/ModelParsingToJson/Question/ViewQuestionPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+namespace FoxfordHack.Models.ModelParsingToJson.Question
+{
+    class ViewQuestionPage
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("available_xp")]
+        public int Xp { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("questions")]
+        public List<BaseQuestion> Question { get; set; }
+    }
+}
